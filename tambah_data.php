15f16e4001803352095c4d516072349dcfa4fd12
rakagigih86 <44790590+rakagigih86@users.noreply.github.com>
2018-11-19 12:31:15 +0800

    Add files via upload

---
+++ tambah_data.php
@@ -0,0 +1,29 @@
+<?php
+    require 'functions.php';
+
+    if(isset($_POST['submit']))
+    {
+
+        var_dump($_post);
+        var_dump($_FILES);
+        die();
+        if(tambah($_POST)>0)
+        {
+            echo "
+            <script>
+                alert('data berhasil disimpan');
+                document.location.href='index.php';
+            </script>
+
+            ";
+        }else{
+            echo "
+            <script>
+                alert('data gagal disimpan');
+                document.location.href='tambah_data.php';
+            </script>";
+            echo "<br>";
+            echo mysqli_error($conn);
+        }
+        
+    }
